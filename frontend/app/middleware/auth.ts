33a5ddb77bc83251f7d720cbeaca35ea627dc733
Timur <trigubov.timka@gmail.com>
2025-12-17 18:22:08 +0300

    delete console.log

--- frontend/app/middleware/auth.ts
+++ frontend/app/middleware/auth.ts
@@ -1,6 +1,5 @@
 export default defineNuxtRouteMiddleware((to) => {
   const token = useCookie('auth_token')
-  console.log('aaa')
   if (!token.value && to.path !== '/auth') {
     return navigateTo('/auth')
   }
